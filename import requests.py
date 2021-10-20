4f07bf80826431ae9043dbdc1639a3d5ff1678f0
Kaygon <cwb1207@snu.ac.kr>
2021-10-20 18:51:08 +0900

    third

--- import requests.py
+++ import requests.py
@@ -3,11 +3,12 @@ from bs4 import BeautifulSoup
 import telegram
 from datetime import datetime
 
+now = datetime.now()
 my_token = '2062236097:AAEbmTzXP0b0gl4BKlHGMuqH2gKqqXyOMlc'
 bot = telegram.Bot(token = my_token)
 chat_id = bot.getUpdates()[-1].message.chat.id #가장 최근에 온 메세지의 chat id를 가져옵니다
 
-url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20211029'
+url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20211030'
 response = requests.get(url)
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
